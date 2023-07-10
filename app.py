6de3fa66d4986c4b56c5077845cff61e570a6843
Mirugwe_Alex <61556630+mirugwe1@users.noreply.github.com>
2023-07-10 15:57:30 +0300

    update

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import pandas as pd
+import os
+import pandas_profiling
+from streamlit_pandas_profiling import st_profile_report
 
 with st.sidebar:
     st.image("https://www.onepointltd.com/wp-content/uploads/2020/03/inno2.png")
@@ -6,15 +10,23 @@ with st.sidebar:
     choice = st.radio("Navigation", ["Upload","Profiling","Modelling", "Download"])
     st.info("This project application helps you build and explore your data.")
 
+if os.path.exists("sourcedata.csv"):
+    df=pd.read_csv("sourcedata.csv",index_col=None)
+
 if choice == "Upload":
+    st.title("Upload your dataset for overview and modelling")
     file = st.file_uploader("Upload Your Dataset Here")
     if file:
-        #activity
+        df = pd.read_csv(file,index_col = None)
+        df.to_csv("sourcedata.csv",index=None)
+        st.dataframe(df)
         pass
 
 
 if choice == "Profiling":
-    pass
+    st.title("Automated Exploratory Data Analysis")
+    profile_report = df.profile_report()
+    st_profile_report(profile_report)
 
 if choice == "Modelling":
     pass
